58ed108762f1c2da78a87ceb400dfb3ea0accc49
mjohny <65822793+MereenaJohny@users.noreply.github.com>
2021-01-25 10:55:37 -0500

    chapter9-lab2-step2

--- Project 1/ch09-project1-start-master/js/chapter09-project01.js
+++ Project 1/ch09-project1-start-master/js/chapter09-project01.js
@@ -1,2 +1,18 @@
 
 /* add code here  */
+window.addEventListener("load", function () {
+    document.querySelectorAll('.hilightable').forEach(item => {
+
+        var hilightable = document.querySelector('.hilightable');
+
+        item.addEventListener('focus', event => {
+            item.classList.remove('highlightable');
+            item.classList.add('highlight');
+        });
+
+        item.addEventListener('blur', event => {
+            item.classList.remove('highlight');
+            item.classList.add('highlightable');
+        });
+    })
+})
