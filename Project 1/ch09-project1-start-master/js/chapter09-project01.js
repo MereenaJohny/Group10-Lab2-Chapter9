6f7987e6502f9127881a3a7daed042c57e715ac7
mjohny <65822793+MereenaJohny@users.noreply.github.com>
2021-01-25 11:02:57 -0500

    chages to step2

--- Project 1/ch09-project1-start-master/js/chapter09-project01.js
+++ Project 1/ch09-project1-start-master/js/chapter09-project01.js
@@ -6,13 +6,13 @@ window.addEventListener("load", function () {
         var hilightable = document.querySelector('.hilightable');
 
         item.addEventListener('focus', event => {
-            item.classList.remove('highlightable');
+            item.classList.remove('hilightable_error');
             item.classList.add('highlight');
         });
 
         item.addEventListener('blur', event => {
             item.classList.remove('highlight');
-            item.classList.add('highlightable');
+            item.classList.add('hilightable_error');
         });
     })
 })
