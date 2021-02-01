43c4cb48733d7991aad2fc50778ea256f7378101
Saneesh Saji <65639829+ssaji1@users.noreply.github.com>
2021-02-01 13:34:43 -0500

    step 4 project 2

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -6,14 +6,26 @@ window.addEventListener('load', function () {// creating event handler after loa
     while (i < imageClick.length) {
         imageClick[i].addEventListener("click", function () {
             var splited = event.target.src.split("images/small/");
-            document.querySelector("#featured>img").src = "images/medium/" + splited[1];
-            document.querySelector("#featured>img").title = event.target.title;
-            document.querySelector("#featured>img").alt = event.target.alt;
+            document.querySelector("#featured > img").src = "images/medium/" + splited[1];
+            document.querySelector("#featured > img").title = event.target.title;
+            document.querySelector("#featured > img").alt = event.target.alt;
             var figuretitle = document.querySelector("#featured > figcaption")
-            figuretitle.style.opacity = "75%";
+            figuretitle.style.opacity = "0%";
             figuretitle.innerHTML = event.target.title;
         })
         i++;
     }
 
+//adding mouse =over and mouseout events.
+var mousein = document.querySelector('#featured img');
+mousein.addEventListener("mouseover", function(event){
+    var figuretitle = document.querySelector("#featured  figcaption");
+    figuretitle.style.opacity = "75%";
+})
+
+var mouseout = document.querySelector('#featured img');
+mousein.addEventListener("mouseout", function(event){
+    var figuretitle = document.querySelector("#featured  figcaption");
+    figuretitle.style.opacity = "0%";
+})
 })
